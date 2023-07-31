be24359207858d83ff18a99d8f93b4bbe10413a0
Chuanqi <chuanqi.sun.dev@gmail.com>
2023-07-30 18:24:51 -0700

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export function getType(input: any, rootName = "Root"): string {
 
 export function getArrayItemType(input: any[], rootName = "Item"): string {
   if (!Array.isArray(input)) throw new Error("Input is not an array");
-  // HACK, when array is empty, use an empty object to make simulate arbitrary item
+  // HACK, when array is empty, use an empty object to simulate arbitrary item
   const root = parse(input.length ? input : [{}]);
   const itemRoot = root.children?.get(0);
   if (!itemRoot) throw new Error("Parser error: Did not find array item");
